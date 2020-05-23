ba61f1bbb80236b0c702512a238e782539b59f65
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-05-23 12:06:00 -0700

    Completed MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeFeatures } from '../actions/carActions'
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => props.removeFeatures(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  return {
+    features: state.features
+  };
+};
+
+
+
+export default connect(mapStateToProps, {removeFeatures})(AddedFeature);
